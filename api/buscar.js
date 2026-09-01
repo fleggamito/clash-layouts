418d09ec2b5011ebd8960313398efb64e014f989
Fleggamito <flegga10@gmail.com>
2026-09-01 14:09:59 -0300

    Update buscar.js

--- api/buscar.js
+++ api/buscar.js
@@ -6,8 +6,8 @@ module.exports = async (req, res) => {
     return res.status(400).json({ error: 'Informe o nível do Centro de Vila.' });
   }
 
-  const query = `TH${cv} war base`;
-  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=date&q=${encodeURIComponent(query)}&type=video&key=${YOUTUBE_API_KEY}`;
+  const query = `TH${cv} base link clash of clans`;
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&order=date&q=${encodeURIComponent(query)}&type=video&key=${YOUTUBE_API_KEY}`;
 
   try {
     const fetch = (await import('node-fetch')).default;
@@ -18,7 +18,7 @@ module.exports = async (req, res) => {
       return res.json({ success: true, data: [] });
     }
 
-    const cocLinkRegex = /https:\/\/link\.clashofclans\.com\/[^\s"]+/g;
+    const cocLinkRegex = /https?:\/\/(link\.clashofclans\.com|clashofclans\.link)[^\s"'>]+/gi;
     const resultados = [];
 
     for (const item of data.items) {
@@ -26,11 +26,14 @@ module.exports = async (req, res) => {
       const links = descricao.match(cocLinkRegex);
 
       if (links && links.length > 0) {
+        // Remove links duplicados no mesmo vídeo
+        const linksUnicos = [...new Set(links)].map(link => link.replace(/[.,;)]+$/, ''));
+
         resultados.push({
           titulo: item.snippet.title,
-          thumbnail: item.snippet.thumbnails.high.url,
+          thumbnail: item.snippet.thumbnails.high ? item.snippet.thumbnails.high.url : item.snippet.thumbnails.default.url,
           videoUrl: `https://www.youtube.com/watch?v=${item.id.videoId}`,
-          layoutLink: links[0]
+          layoutLinks: linksUnicos // Guarda a lista com todos os links achados no vídeo
         });
       }
     }
